1448e2eddafb4c4d791e0db3fe98241a26d93986
nmfirdausw <nmfirdausw@gmail.com>
2026-09-18 15:18:36 +0800

    feat(keymaps): add navigation and editing keymaps

---
+++ lua/keymaps.lua
@@ -0,0 +1,43 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+local map = vim.keymap.set
+
+-- Move by visual line when wrapped, by count otherwise
+map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+
+-- Window navigation
+map("n", "<C-h>", "<C-w>h", { desc = "Go to left window" })
+map("n", "<C-j>", "<C-w>j", { desc = "Go to lower window" })
+map("n", "<C-k>", "<C-w>k", { desc = "Go to upper window" })
+map("n", "<C-l>", "<C-w>l", { desc = "Go to right window" })
+
+-- Window resizing
+map("n", "<C-Up>", "<cmd>resize +2<CR>", { desc = "Increase window height" })
+map("n", "<C-Down>", "<cmd>resize -2<CR>", { desc = "Decrease window height" })
+map("n", "<C-Left>", "<cmd>vertical resize -2<CR>", { desc = "Decrease window width" })
+map("n", "<C-Right>", "<cmd>vertical resize +2<CR>", { desc = "Increase window width" })
+
+-- Keep selection after shifting indentation
+map("x", "<", "<gv", { desc = "Shift left" })
+map("x", ">", ">gv", { desc = "Shift right" })
+
+-- Move selected lines up/down
+map("x", "J", ":move '>+1<CR>gv=gv", { desc = "Move selection down" })
+map("x", "K", ":move '<-2<CR>gv=gv", { desc = "Move selection up" })
+
+-- Keep cursor centered while scrolling/searching
+map("n", "<C-d>", "<C-d>zz", { desc = "Scroll down" })
+map("n", "<C-u>", "<C-u>zz", { desc = "Scroll up" })
+map("n", "n", "nzzzv", { desc = "Next search result" })
+map("n", "N", "Nzzzv", { desc = "Previous search result" })
+
+-- Paste over a visual selection without overwriting the unnamed register
+map("x", "p", '"_dP', { desc = "Paste" })
+
+-- Yank to end of line, matching D and C
+map("n", "Y", "y$", { desc = "Yank to end of line" })
+
+-- Clear search highlight
+map("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear search highlight" })
